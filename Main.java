d456a3d49497efac3c0dbd3d44ba89c3a17a1597
Elizaveta Moskalenko <54900460+moskalenko9381@users.noreply.github.com>
2021-07-05 00:43:24 +0300

    Update Main.java
    Добавлена возможность считывания из файла

--- Main.java
+++ Main.java
@@ -1,15 +1,43 @@
 package binary_trees;
 
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws FileNotFoundException {
 
         Scanner console = new Scanner(System.in);
-        String input ;
-        System.out.println("Введите запись дерева в виде строки, где '/' - пустой элемент:");
-        input = console.nextLine();
+        String input = "";
+        String fileName;
+
+        System.out.println("Выберите:\n1. Ввод списка с консоли\n2. Ввод списка с файла");
+        int choice = console.nextInt();
+
+        if (choice == 1) {
+                System.out.println("Введите запись дерева в виде строки, где '/' - пустой элемент:");
+                input = console.next();
+                System.out.println(input + "*");
+            }
+        else if (choice == 2){
+                System.out.println("Введите полный путь до файла:");
+                fileName = console.next();
+                File file = new File(fileName);
+                Scanner scanner = new Scanner(file);
+                while (scanner.hasNext()) {
+                    if (scanner.hasNextLine()) {
+                        input = scanner.nextLine();
+                    } else {
+                        scanner.next();
+                    }
+                }
+            }
+        else {
+                System.out.println("Вы должны ввести 1 или 2");
+                return;
+            }
+
 
 
         if (2*count(input, '/') - input.length() != 1 || input.charAt(0) == '/') {  //проверка на то, что '/' на одну больше, чем остальных символов
@@ -25,6 +53,10 @@ public class Main {
         treePrint(head,root);
         head.listPrint();
     }
+
+
+
+
 
     static void recTreePrint(Elem node) {
         if (node == null) {
@@ -74,4 +106,3 @@ public class Main {
     }
 
 }
-
